62fb67ebd54376aa0d3e99adf744ffc756a8b9e8
Serhii <serge5709244@gmail.com>
2021-09-22 16:40:30 -0400

    Added joint for head

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     private Vector3 currentLookTarget = Vector3.zero;
 
+    public Rigidbody head;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,38 +29,13 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-       /* Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
  0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
             // TODO
         }
         else
-        {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
-        }
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
-
-        if (Physics.Raycast(ray, out hit, 1000, layerMask,
-        QueryTriggerInteraction.Ignore))
-        {
-            if (hit.point != currentLookTarget)
-            {
-                currentLookTarget = hit.point;
-            }
-
-            // 1
-            Vector3 targetPosition = new Vector3(hit.point.x,
-             transform.position.y, hit.point.z);
-            // 2
-            Quaternion rotation = Quaternion.LookRotation(targetPosition -
-             transform.position);
-            // 3
-            transform.rotation = Quaternion.Lerp(transform.rotation,
-             rotation, Time.deltaTime * 10.0f);
-        }*/
     }
 }
